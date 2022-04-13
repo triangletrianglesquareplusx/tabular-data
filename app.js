65e182324cac9ef8af1f043d5872f3ac5d1eb99d
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-13 13:40:41 +0300

    chart implemented

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 'use strict'
 
-getData();
+const xlabels = [];
+const ytemps = [];
+
+chartIt();
+
+
+async function chartIt(){
+
+    await getData();
+    const ctx = document.getElementById('chart').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xlabels,
+            datasets: [{
+                label: 'Global Average Temperature',
+                data: ytemps,
+                backgroundColor: ['rgba(255, 99, 132, 0.2)',],
+                borderColor: ['rgba(255, 99, 132, 1)'],
+                borderWidth: 1}]
+            }
+    });
+}
+
+
 
 async function getData(){
     const response = await fetch('ZonAnn.Ts+dSST.csv');
@@ -12,7 +37,9 @@ async function getData(){
     rows.forEach(element=>{
         const row = element.split(',');
         const year = row[0];
+        xlabels.push(year);
         const temp = row[1];
+        ytemps.push(parseFloat(temp) + 14);
         console.log(year, temp);
     })
 }
