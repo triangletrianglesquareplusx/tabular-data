a1402bbbd87ad51256994f98dc481399e9ce7436
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-04-13 13:01:31 +0300

    basic visual and parsing of csv

--- app.js
+++ app.js
@@ -5,5 +5,14 @@ getData();
 async function getData(){
     const response = await fetch('ZonAnn.Ts+dSST.csv');
     const data = await response.text();
-    console.log(data);
+    //console.log(data);
+
+    const rows = data.split('\n').slice(1);
+    //console.log(rows);
+    rows.forEach(element=>{
+        const row = element.split(',');
+        const year = row[0];
+        const temp = row[1];
+        console.log(year, temp);
+    })
 }
